65672f90a026fa163d2e043fcab00fa7ee7ec13b
spti <tobetodee@gmail.com>
2022-04-22 16:39:41 +0300

    drop htmlat-raw dep

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {readFile, writeFile} from 'fs/promises'
 import {JSDOM} from 'jsdom'
 import sanitize from 'sanitize-html'
-import {convert as _convert_raw, TAGS as TAGS_RAW} from 'htmlat-raw/src/index.js'
+// import {convert as _convert_raw, TAGS as TAGS_RAW} from 'htmlat-raw/src/index.js'
 import {convert as _convert_rich, spanToTextTextNodes, TAGS as TAGS_RICH} from 'htmlat-rich/src/index.js'
 
 
@@ -14,19 +14,17 @@ function log(...args) {
 
 function convert(i, options) {
     const Dom = new JSDOM("<!DOCTYPE html><main></main>")
-    const dom = options.raw
-        ? _convert_raw(i, Dom.window.document)
-        : _convert_rich(i, Dom.window.document, {
+    const dom = _convert_rich(i, Dom.window.document, {
             // see 'Sanitizing encoded content doesn't work' in htmlat-rich
             spanTextNodes: true
-        })
+    })
 
     Dom.window.document.querySelector('main').appendChild(dom)
 
     // console.log("converted, with span text nodes", Dom.window.document.querySelector('main').innerHTML)
 
     const domStr = sanitize(Dom.window.document.querySelector('main').innerHTML, {
-        allowedTags: options.raw ? TAGS_RAW : TAGS_RICH,
+        allowedTags: TAGS_RICH,
         allowedAttributes: {'*': ['data-*'], ...sanitize.defaults.allowedAttributes},
     })
 
