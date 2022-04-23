62324d918dbeb50415d47ce0457ea5510d2f023b
spti <tobetodee@gmail.com>
2022-04-23 14:37:04 +0300

    optionally provide custom html inside which to place the content

--- readme.md
+++ readme.md
@@ -14,8 +14,40 @@ import {convertFile} from 'htmlat-nodejs'
 convertFile('src/my-blogpost.semtext', 'dist/my-blogpost.html', {wrap: true})
 ```
 
-### `wrap` option
-Whether to wrap the resulting html in an html document (i.e., `<!DOCTYPE html><html><body>your content here</body></html>`).
+### Wrapping content in a document
+You can provide custom html to wrap your content with. E.g., with
+```html
+<html>
+    <head>
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+    <body>
+        <main data-slot></main>
+    </body>
+</html>
+```
+your content will be placed inside the `main` element.
 
-## cli
+To provide the wrap:
+```javascript
+convert('(p){{my content}}', '<html><head></head><body></body></html>')
+convertFile('src/my-blogpost.semtext', 'dist/my-blogpost.html', 'src/custom-wrap.html')
+```
+
+#### Allowed tags
+The following tags are allowed in a wrap: `html`, `body`, `head`, `link`, `meta`, `header`, `nav`, `aside`, `main`, `script`.
+
+#### `data-slot`
+The content will be placed inside the element, which has the `data-slot` attribute. The wrap must have an element with this attribute.
+
+#### `script`
+The `script` tags can only be used to import scripts (e.g., `<script src="index.js"></script>`). Any `script` that has content will be removed.
+
+### cli
 [`htmlat-cli`](#) provides a nice cli for this module.
+
+### using paths in `script` `src`
+`sanitize-html` won't allow simple paths (e.g., `scripts/index.js`) in the `src` attribute; it only allows urls.
+To be able to use such paths, you have to specify the path as part of a url, e.g. `https://scripts/index.js` and set the `data-url-to-path` attribute.
+
+E.g., `<script src="https://scripts/index.js" data-url-to-path></script>`.
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import path from 'path'
+// import URL from 'url'
 import {readFile, writeFile} from 'fs/promises'
+
 import {JSDOM} from 'jsdom'
 import sanitize from 'sanitize-html'
 // import {convert as _convert_raw, TAGS as TAGS_RAW} from 'htmlat-raw/src/index.js'
@@ -12,37 +15,82 @@ function log(...args) {
     console.log(...args)
 }
 
-function convert(i, options) {
-    const Dom = new JSDOM("<!DOCTYPE html><main></main>")
+function sanitizeWrap(wrap) {
+    return sanitize(wrap, {
+        allowedTags: ['html', 'body', 'head', 'link', 'meta', 'header', 'nav', 'aside', 'main', 'script',],
+        allowVulnerableTags: true,
+        allowedAttributes: false,
+        exclusiveFilter: (frame) => {
+            return 'script' === frame.tag && frame.text.trim().length
+        },
+    })
+}
+
+function sanitizeContent(content) {
+    return sanitize(content, {
+        allowedTags: TAGS_RICH,
+        allowedAttributes: {'*': ['data-*'], ...sanitize.defaults.allowedAttributes},
+    })
+}
+
+// see using paths in script src
+function replaceScriptSrc(els) {
+    els.forEach(el => {
+        if (!el.getAttribute('src')) {
+            el.removeAttribute('data-url-to-path')
+            return
+        }
+
+        const url = new URL(el.getAttribute('src'))
+        el.setAttribute('src', `${url.host}${url.pathname}${url.search}${url.hash}`)
+        el.removeAttribute('data-url-to-path')
+    })
+}
+
+function convert(i, wrap, options) {
+    const _wrap = wrap ? sanitizeWrap(wrap) : "<!DOCTYPE html><main></main>"
+
+    console.log("convert, _wrap:", _wrap);
+
+    const Dom = new JSDOM(_wrap)
+    if (wrap) {
+        if (!Dom.window.document.querySelector('[data-slot]')) throw new Error("wrap content must have an element with data-slot attribute")
+        replaceScriptSrc(Dom.window.document.querySelectorAll('[data-url-to-path]'))
+    }
+
+    const slotSelector = wrap ? '[data-slot]' : 'main'
+
     const dom = _convert_rich(i, Dom.window.document, {
             // see 'Sanitizing encoded content doesn't work' in htmlat-rich
             spanTextNodes: true
     })
 
-    Dom.window.document.querySelector('main').appendChild(dom)
+    Dom.window.document.querySelector(slotSelector).appendChild(dom)
 
     // console.log("converted, with span text nodes", Dom.window.document.querySelector('main').innerHTML)
 
-    const domStr = sanitize(Dom.window.document.querySelector('main').innerHTML, {
-        allowedTags: TAGS_RICH,
-        allowedAttributes: {'*': ['data-*'], ...sanitize.defaults.allowedAttributes},
-    })
+    const domStr = sanitizeContent(Dom.window.document.querySelector(slotSelector).innerHTML)
 
     // console.log("sanitized, with span text nodes", domStr)
 
     // spanToTextTextNodes works with dom
-    Dom.window.document.querySelector('main').innerHTML = domStr
-    spanToTextTextNodes(Dom.window.document.querySelector('main'), Dom.window.document)
+    Dom.window.document.querySelector(slotSelector).innerHTML = domStr
+    spanToTextTextNodes(Dom.window.document.querySelector(slotSelector), Dom.window.document)
 
     // console.log("with span text nodes converted to Text nodes", Dom.window.document.documentElement.outerHTML)
 
-    return options.wrap ? Dom.serialize() : Dom.window.document.querySelector('main').innerHTML
+    return wrap ? Dom.serialize() : Dom.window.document.querySelector('main').innerHTML
 }
 
-async function convertFile(src, dest, options) {
+async function convertFile(src, dest, wrap, options) {
     const i = await readFile(src, 'utf8')
+    const wrapContent = wrap
+        ? await readFile(wrap, 'utf8')
+        : null
 
-    const o = convert(i, options || {})
+    console.log("convertFile, wrapContent", wrapContent)
+
+    const o = convert(i, wrapContent, options || {})
 
     await writeFile(dest, o)
 }
